8b9c85e804718c28fff14e4735e8cab54fae2f0b
Ryan Moeller <ryan@freqlabs.com>
2016-10-11 00:11:25 -0700

    Improve readability of scraper.py
    `fetch_icalendars()` now catches parse errors and returns None.
    
    Some repeated checks were broken out into the two functions
    `has_location()` and `has_contact_info()`.

--- Scraper/scraper.py
+++ Scraper/scraper.py
@@ -8,9 +8,11 @@ import vobject
 import psycopg2
 from psycopg2.extensions import AsIs
 
+
 # This import comes from secrets.py in the same directory as this script.
 # If that file does not exist, create one using secrets.py-sample as a template.
 from secrets import credentials
+
 
 CALENDAR_URLS = [
     #"https://25livepub.collegenet.com/calendars/Highlighted_Event.ics",
@@ -31,6 +33,7 @@ DATABASE_CONNECT_ARGS = dict(
     **credentials
 )
 
+
 # Normally, values passed to the database cursor get quoted and escaped
 # so that arbitrary SQL cannot be injected through values.  The AsIs class
 # is a special value that renders into the query unquoted when passed as a
@@ -41,12 +44,21 @@ DEFAULT = AsIs('DEFAULT')
 
 
 def fetch_icalendar(url):
-    resp = requests.get(url)
-    if resp.ok:
-        return vobject.readOne(resp.text)
+    """
+    Returns a parsed iCalendar file as a vobject if the URL was able to be
+    fetched and parsed, otherwise None.
+    """
+    calendar = None
+    response = requests.get(url)
+    if response.ok:
+        try:
+            calendar = vobject.readOne(response.text)
+        except vobject.base.ParseError as e:
+            print ("Failed to parse calendar at URL: " + url)
     else:
         print("Failed to fetch URL: " + url)
-        return None
+
+    return calendar
 
 
 #
@@ -55,7 +67,7 @@ def fetch_icalendar(url):
 
 def pad(iterable, end):
     """
-    Returns the sequence elements and then returns end indefinitely.
+    Yields the sequence of elements in iterable then yields end indefinitely.
 
     Inspired by https://docs.python.org/2/library/itertools.html#recipes
     """
@@ -64,7 +76,7 @@ def pad(iterable, end):
 
 def padded(func):
     """
-    Pads the result of func() with repeated end value.
+    Decorator that pads the result of func() with repeated None.
     """
     @wraps(func)
     def f(*args, **kwargs):
@@ -74,7 +86,8 @@ def padded(func):
 
 def headonly(func):
     """
-    Returns only the first item from an iterator.
+    Decorator that causes only the first item from the iterator returned by
+    func() to be returned.
     """
     @wraps(func)
     def f(*args, **kwargs):
@@ -99,14 +112,14 @@ def get_value(key, event):
 
 @headonly
 @padded
-def get_custom_value(fid, event):
+def get_custom_value(field_id, event):
     """
     Returns a generator that yields the values for the given custom field
     in the specified event.
     """
-    int_fid = int(fid)
+    int_field_id = int(field_id)
     return (field.value for field in event.x_trumba_customfield_list
-            if int(field.params['ID'][0]) == int_fid)
+            if int(field.params['ID'][0]) == int_field_id)
 
 
 # Helper for the getters below
@@ -133,6 +146,7 @@ def get_event_id(event):
     event_id = value.rsplit('/')[-1]
     return event_id
 
+
 def get_categories(event):
     """
     Returns as a list the set of unique categories specified for an event.
@@ -147,6 +161,7 @@ def get_categories(event):
     custom_list = custom.split(', ')
     return list(set(custom_list + basic))
 
+
 def get_all_day_event(event):
     """
     Returns a Python boolean True or False for this field if present,
@@ -155,6 +170,7 @@ def get_all_day_event(event):
     value = get_value("x-microsoft-alldayevent", event)
     boolval = check_bool(value)
     return DEFAULT if boolval is None else boolval
+
 
 def get_open_to_public(event):
     """
@@ -179,10 +195,11 @@ def get_record(event):
     Returns a dict mapping column names to values from the given event.
     """
     # Convenience functions that supply the event to the inner functions.
-    def getter(field):
-        return get_value(field, event)
-    def custom_getter(fid):
-        return get_custom_value(fid, event)
+    def getter(field_name):
+        return get_value(field_name, event)
+
+    def custom_getter(field_id):
+        return get_custom_value(field_id, event)
 
     return {
         "event_id": get_event_id(event),
@@ -197,15 +214,16 @@ def get_record(event):
         "end": getter("dtend"),
 
         "event_type": custom_getter(12),
-        "general_admission_fee": custom_getter(3124),
-        "student_admission_fee": custom_getter(3111),
         "website_url": custom_getter(3109),
+        "student_admission_fee": custom_getter(3111),
+        "general_admission_fee": custom_getter(3124),
         "ticket_sales_url": custom_getter(13402),
 
         "contact_name": coalesce(custom_getter(13404), ''),
         "contact_phone": coalesce(custom_getter(13405), ''),
         "contact_email": coalesce(custom_getter(13406), ''),
     }
+
 
 def get_records(urls):
     """
@@ -216,16 +234,38 @@ def get_records(urls):
     is a flat sequence of events, not separated into different calendars.
     """
     for url in urls:
-        cal = fetch_icalendar(url)
-        if cal is None:
+        calendar = fetch_icalendar(url)
+        if calendar is None:
             continue
-        for event in cal.vevent_list:
+        for event in calendar.vevent_list:
             record = get_record(event)
             yield record
 
 
 #
-# Some generic helpers for working with iterators
+# Tests for checking if record fields exist
+#
+
+def has_contact_info(record):
+    """
+    Returns True if any of the contact info fields exist, otherwise False.
+    """
+    return (
+        record.get('contact_name', None) != None
+        or record.get('contact_phone', None) != None
+        or record.get('contact_email', None) != None
+    )
+
+
+def has_location(record):
+    """
+    Returns true if the location field exists, otherwise False.
+    """
+    return record.get('location', None) != None
+
+
+#
+# Generic helpers for working with iterators
 #
 
 def count_iter(it):
@@ -235,6 +275,7 @@ def count_iter(it):
     Does not work for infinite iterators.
     """
     return sum(1 for _ in it)
+
 
 def take(n, it):
     """
@@ -249,7 +290,7 @@ def take(n, it):
 # Some helpers for functions using SQL
 #
 
-def make_values_sql(fields):
+def make_values_sql(*fields):
     """
     Returns a string listing the columns provided in fields, in the format
     required by the psycopg2 cursor.
@@ -272,6 +313,7 @@ def make_values_sql(fields):
         )
     )
 
+
 def uses_values_fields(*fields):
     """
     Decorator for specifying field names to a statement template.
@@ -281,10 +323,11 @@ def uses_values_fields(*fields):
     def wrapper(func):
         @wraps(func)
         def f(*args, **kwargs):
-            values_sql = make_values_sql(fields)
+            values_sql = make_values_sql(*fields)
             return func(values_sql, *args, **kwargs)
         return f
     return wrapper
+
 
 def uses_statement_template(statement_template):
     """
@@ -327,6 +370,7 @@ def make_common_insert(statement_template, *fields):
     @uses_statement_template(statement_template)
     def common_insert(statement, cursor, event):
         cursor.execute(statement, event)
+
     return common_insert
 
 
@@ -343,6 +387,7 @@ insert_contact = make_common_insert(
     'contact_name', 'contact_phone', 'contact_email'
 )
 
+
 insert_location = make_common_insert(
     """
     INSERT INTO ssucalendar.locations(name)
@@ -351,6 +396,7 @@ insert_location = make_common_insert(
     """,
     'location'
 )
+
 
 insert_event_type = make_common_insert(
     """
@@ -375,6 +421,7 @@ INSERT INTO ssucalendar.categories(name)
 """
     cursor.executemany(statement, ((c,) for c in event['categories']))
 
+
 # The events table has a lot of fields.  Rather than repeating them all and
 # having to make sure they're in the same order everywhere, we use one tuple
 # to keep track of the fields and then generate the various bits of SQL from
@@ -390,6 +437,7 @@ event_fields_sql = ', '.join(event_fields)
 set_fields_sql = ', '.join(event_fields[1:])
 set_values_sql = ', '.join('EXCLUDED.' + field for field in event_fields[1:])
 
+
 @uses_values_fields(*event_fields)
 @uses_statement_template("""
 INSERT INTO ssucalendar.events({event_fields:s})
@@ -402,32 +450,35 @@ INSERT INTO ssucalendar.events({event_fields:s})
     set_values = set_values_sql
 ))
 def insert_event(statement, cursor, event):
-    # Set location_id in the event dict.  Not all events have a location, so
-    # the value may be None instead.
-    if event.get('location', None) is None:
-        event['location_id'] = None
-    else:
+    # Set location_id in the event dict.  Not all events have a location,
+    # so the value may be None instead.
+    if has_location(event):
         cursor.execute(
-            """SELECT location_id FROM ssucalendar.locations WHERE name = %(location)s;""",
+            """SELECT location_id FROM ssucalendar.locations
+                   WHERE name = %(location)s;""",
             event
         )
         event['location_id'] = cursor.fetchone()
+    else:
+        event['location_id'] = None
 
     # Set event_type_id in the event dict.  This value is always present.
     cursor.execute(
-        """SELECT event_type_id FROM ssucalendar.event_types WHERE name = %(event_type)s;""",
+        """SELECT event_type_id FROM ssucalendar.event_types
+               WHERE name = %(event_type)s;""",
         event
     )
     event['event_type_id'] = cursor.fetchone()
 
-    # Set contact_id in the event dict. This value may be None if not present.
-    if event.get('contact_name', None) != None \
-      or event.get('contact_phone', None) != None \
-      or event.get('contact_email', None) != None:
+    # Set contact_id in the event dict.  Not all events have contact info,
+    # so this value may be None instead.
+    if has_contact_info(event):
         cursor.execute(
             """SELECT contact_id FROM ssucalendar.contacts
                    WHERE (name, phone, email)
-                       = (%(contact_name)s, %(contact_phone)s, %(contact_email)s);""",
+                       = (%(contact_name)s,
+                          %(contact_phone)s,
+                          %(contact_email)s);""",
             event
         )
         event['contact_id'] = cursor.fetchone()
@@ -438,6 +489,7 @@ def insert_event(statement, cursor, event):
     event['"end"'] = event['end']
 
     cursor.execute(statement, event)
+
 
 def insert_event_categories(cursor, event):
     statement = \
@@ -450,7 +502,7 @@ INSERT INTO ssucalendar.event_categories(event_id, category_id)
     SELECT %(event_id)s, id FROM cat
     ON CONFLICT DO NOTHING;
 """
-    # This is a generator that yields a dict(event_id,name) for every
+    # This is a generator that yields a dict(event_id, name) for every
     # category in the event.  There are many categories per event, and many
     # events, so this table is building a many-to-many relationship.
     values = (
@@ -467,11 +519,9 @@ INSERT INTO ssucalendar.event_categories(event_id, category_id)
 
 def populate_database(cursor):
     for record in get_records(CALENDAR_URLS):
-        if record.get('contact_name', None) != None \
-          or record.get('contact_phone', None) != None \
-          or record.get('contact_email', None) != None:
+        if has_contact_info(record):
             insert_contact(cursor, record)
-        if record.get('location', None) != None:
+        if has_location(record):
             insert_location(cursor, record)
         insert_event_type(cursor, record)
         insert_categories(cursor, record)
