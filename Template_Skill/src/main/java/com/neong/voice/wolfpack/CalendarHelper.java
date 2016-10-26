42ddd44e04416e41dc6901881000b832b9b5c9cb
Ryan Moeller <ryan@freqlabs.com>
2016-10-25 18:43:21 -0700

    Refactor CalendarConversation and CalendarHelper
    This change breaks the long if-else chain for dispatching to the
    appropriate handlers down into smaller, separate switch statements.  The
    division helps reinforce what state the session is expected to be for
    each handler.
    
    Magic strings have been replaced with appropriately named constants in
    several places.  The state integer is now an enum with descriptive
    identifiers.
    
    Many repeated response formats have been reduced into a few simple helper
    functions.
    
    One handler has been converted to the new PreparedStatement style of
    querying the database, as a demonstration of the correct usage.
    
    The CalendarHelper class has been promoted to be responsible for all
    special SSML formatting logic for calendar fields.
    
    A generic event formatter has been added which takes an array of the
    desired calendar fields, a Map of events, and an index, returning a
    String of the fields described with appropriate surrounding grammar and
    SSML markup.
    
    The listEvents() method now takes an array of fields as well, internally
    relying on the new formatter to build the final string.
    
    `<speak>` tags are no longer added by any tags in this class, leaving
    the responsibility on the caller to appropriately build their final
    response SSML.

--- Template_Skill/src/main/java/com/neong/voice/wolfpack/CalendarHelper.java
+++ Template_Skill/src/main/java/com/neong/voice/wolfpack/CalendarHelper.java
@@ -7,41 +7,103 @@ import java.time.format.DateTimeFormatter;
 import java.util.Map;
 import java.util.Vector;
 
-public  class CalendarHelper {
-	public static boolean isCategorySupported(String category){
-		String [] supportedCategories = {"SportsCategoryIntent", "ArtsAndEntertainmentCategoryIntent", "LecturesCategoryIntent",  "ClubsCategoryIntent"};
-		for (int i = 0; i < 4; i++){
+
+public class CalendarHelper {
+	private static final DateTimeFormatter DAY_FORMATTER = DateTimeFormatter.ofPattern("EEEE");
+	private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("????MMdd");
+	private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("h:mm a");
+	private static final ZoneId PST_ZONEID = ZoneId.of("America/Los_Angeles");
+
+	public enum EventField { DATE, TIME, SUMMARY, LOCATION };
+
+
+	public static boolean isCategorySupported(String category) {
+		final String[] supportedCategories = {
+			"SportsCategoryIntent", "ArtsAndEntertainmentCategoryIntent",
+			"LecturesCategoryIntent",  "ClubsCategoryIntent"
+		};
+
+		for (int i = 0; i < 4; i++)
 			if (category == supportedCategories[i])
 				return true;
-		}
+
 		return false;
 	}
-	
-	public static String listEvents(Map<String, Vector<Object>> results, String eventDate){
 
-		String response;
-		final DateTimeFormatter DATEFORMATTER = DateTimeFormatter.ofPattern("????MMdd");
-		final DateTimeFormatter TIMEFORMATTER = DateTimeFormatter.ofPattern("h:mm a");
-		final ZoneId PST = ZoneId.of("America/Los_Angeles");
-		Timestamp firstEventStart = (Timestamp) results.get("start").get(0);
-		ZonedDateTime zonedDateTime = firstEventStart.toInstant().atZone(PST);
-		
-		response = "<speak> Okay, here's what i found for <say-as interpret-as=\"date\">" + eventDate + "</say-as>. " +
-			"<break time=\".25s\"/>";
-		for(int i = 0; i < results.get("summary").size() ; i++){
-			String summary = (String) results.get("summary").get(i);
-			Timestamp start = (Timestamp) results.get("start").get(i);
-			zonedDateTime = start.toInstant().atZone(PST);
-			String formattedStart = "<say-as interpret-as=\"time\">" + zonedDateTime.format(TIMEFORMATTER) + "</say-as>";
-			if(i == results.get("summary").size() - 1)
-				response += " and ";
-			response += summary + " at " + formattedStart + "<break time=\".25s\"/>";
 
+	public static String formatEventSsml(int index, Map<String, Vector<Object>> events, EventField[] fields) {
+		String ssml = "<s>";
+
+		for (EventField field : fields) {
+			switch (field) {
+			case SUMMARY:
+				final String summary = (String) events.get("summary").get(index);
+				ssml += summary;
+				break;
+
+			case DATE: {
+				final Timestamp start = (Timestamp) events.get("start").get(index);
+				ssml += "on " + formatDateSsml(start);
+				break;
+			}
+
+			case TIME: {
+				final Timestamp start = (Timestamp) events.get("start").get(index);
+				ssml += "at " + formatTimeSsml(start);
+				break;
+			}
+
+			case LOCATION:
+				final String location = (String) events.get("location").get(index);
+				ssml += "at " + location;
+				break;
+
+			default:
+				break;
+			}
+
+			ssml += " ";
 		}
-		response += "</speak>";
-		response = response.replaceAll("&", " and ");
+
+		ssml += "</s>";
+
+		return ssml.replaceAll("&", " and ");
+	}
+
+
+	public static String formatDateSsml(Timestamp when) {
+		final ZonedDateTime zonedDateTime = when.toInstant().atZone(PST_ZONEID);
+		final String day = zonedDateTime.format(DAY_FORMATTER);
+		final String date = zonedDateTime.format(DATE_FORMATTER);
+
+		return day + ", <say-as interpret-as=\"date\">" + date + "</say-as>";
+	}
+
+
+	public static String formatTimeSsml(Timestamp when) {
+		final ZonedDateTime zonedDateTime = when.toInstant().atZone(PST_ZONEID);
+		final String time = zonedDateTime.format(TIME_FORMATTER);
+
+		return "<say-as interpret-as=\"time\">" + time + "</say-as>";
+	}
+
+
+	public static String formatLocationSsml(String location) {
+		if (location == null)
+			location = "Sonoma State University";
+
+		return location;
+	}
+
+
+	public static String listEvents(Map<String, Vector<Object>> events, EventField[] fields) {
+		String response = "";
+
+		final int eventsLength = events.get("summary").size();
+
+		for (int i = 0; i < eventsLength; i++)
+			response += formatEventSsml(i, events, fields);
+
 		return response;
 	}
-	
 }
-
