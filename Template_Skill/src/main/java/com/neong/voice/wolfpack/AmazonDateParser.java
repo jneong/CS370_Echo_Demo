0b75241af943544ecbf3d8a8d0d38d345e33d893
erich <bucher.erich@gmail.com>
2016-11-08 00:39:33 -0800

    Add method documentation

--- Template_Skill/src/main/java/com/neong/voice/wolfpack/AmazonDateParser.java
+++ Template_Skill/src/main/java/com/neong/voice/wolfpack/AmazonDateParser.java
@@ -13,7 +13,14 @@ import org.apache.commons.lang3.tuple.ImmutablePair;
 public class AmazonDateParser {
 	private static final int SUNDAY = 1;
 
-
+	/**
+	 * Parses the givenDate and comes up with two different dates related to the
+	 * String. The first date should be the first day of the given range. The
+	 * second date will be the date after the last day of the range.
+	 * 
+	 * @param givenDate	An ISO 8601 formatted string representing a date or range
+	 * 					of dates from Amazon.
+	 */
 	public static ImmutablePair<Date, Date> parseAmazonDate(final String givenDate) {
 		final ImmutablePair<Date, Date> result;
 
@@ -42,6 +49,11 @@ public class AmazonDateParser {
 	}
 
 
+	/**
+	 * @param date	A string representing a date, formatted as yyyy-MM-dd
+	 * @return 		The first date object returned is the date represented
+	 * 				by the string. The second date is the next day.
+	 */
 	public static ImmutablePair<Date, Date> handleDay(final String date){
 		final Timestamp firstDay = Timestamp.valueOf(date + " 00:00:00");
 		final Date begin = new Date(firstDay.getTime());
@@ -56,6 +68,19 @@ public class AmazonDateParser {
 	}
 
 
+	/**
+	 * Given a string representing a week from Amazon, the function will get the
+	 * Sunday at the beginning of that week. It is assumed that the first week
+	 * of the year is the first one that contains a Thursday. If the method is
+	 * called on a Sunday, (as in the user asks on a Sunday), the method will
+	 * look for the upcoming week instead of the previous week like regular ISO
+	 * 8601 would require.
+	 * 
+	 * @param date	A string representing a date, formatted as yyyy-Wnum.
+	 * 				Example: 2016-W44
+	 * @return	The first date object will be the Sunday of the week. The second
+	 * 			date will be the Sunday after the given week.
+	 */
 	public static ImmutablePair<Date, Date> handleWeek(final String date){
 		final String[] pieces = date.split("-W");
 		final int year = Integer.parseInt(pieces[0]);
@@ -79,6 +104,11 @@ public class AmazonDateParser {
 	}
 
 
+	/**
+	 * @param date	A string representing a date in the format of yyyy-MM
+	 * @return		The first date will be the first day of the given month.
+	 * 				The second date will be the first date of the next month.
+	 */
 	public static ImmutablePair<Date, Date> handleMonth(final String date){
 		final String[] pieces = date.split("-");
 		final int year = Integer.parseInt(pieces[0]);
@@ -97,7 +127,10 @@ public class AmazonDateParser {
 		return ImmutablePair.of(begin, end);
 	}
 
-
+	
+	/**
+	 * @return The day number of the week. Sunday is 1 and Saturday is 6.
+	 */
 	private static int todayNumber(){
 		final Calendar today = Calendar.getInstance();
 		final int dayOfWeek = today.get(Calendar.DAY_OF_WEEK);
@@ -105,7 +138,7 @@ public class AmazonDateParser {
 		return dayOfWeek;
 	}
 
-
+	
 	private static Date calendarToDate(final Calendar calendar) {
 		// Calendar -> java.util.Date -> long
 		final long time = calendar.getTime().getTime();
